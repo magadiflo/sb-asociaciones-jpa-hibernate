1cfec69c5017085e1a27b63dbaeeca3b8490e69e
Martín <magadiflo@gmail.com>
2022-11-18 19:04:15 -0500

    ManyToOne - Unidireccional V1 - Guardar Factura

--- src/main/java/com/asociaciones/jpa/app/unidireccional/v1/ManyToOne/resource/FacturaResource.java
+++ src/main/java/com/asociaciones/jpa/app/unidireccional/v1/ManyToOne/resource/FacturaResource.java
@@ -1,10 +1,9 @@
 package com.asociaciones.jpa.app.unidireccional.v1.ManyToOne.resource;
 
+import com.asociaciones.jpa.app.unidireccional.v1.ManyToOne.entity.Factura;
 import com.asociaciones.jpa.app.unidireccional.v1.ManyToOne.repository.IFacturaRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/unidireccional/v1/many-to-one/facturas")
@@ -19,5 +18,10 @@ public class FacturaResource {
     @GetMapping
     public ResponseEntity<?> listarFacturas() {
         return ResponseEntity.ok(this.facturaRepository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> guardarFactura(@RequestBody Factura factura) {
+        return ResponseEntity.ok(this.facturaRepository.save(factura));
     }
 }
