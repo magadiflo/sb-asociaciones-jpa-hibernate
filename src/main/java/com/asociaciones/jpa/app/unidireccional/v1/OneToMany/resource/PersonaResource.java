750d19ff857e591d2dc462a034ed7a7e7a8eb3ec
Martín <magadiflo@gmail.com>
2022-11-18 20:24:39 -0500

    OneToMany - Unidireccional V1 - Comentario ¿Porqué se guarda las direccionesDeafulta asociados a PersonaDefault?

--- src/main/java/com/asociaciones/jpa/app/unidireccional/v1/OneToMany/resource/PersonaResource.java
+++ src/main/java/com/asociaciones/jpa/app/unidireccional/v1/OneToMany/resource/PersonaResource.java
@@ -23,6 +23,35 @@ public class PersonaResource {
         return ResponseEntity.ok(this.personaRepositoryDefault.findAll());
     }
 
+    /**
+     * guardarPersonaConDirecciones(...), aquí mandaremos los datos de la PersonaDefault
+     * que se guardarán en la BD. Además, dentro de ese mismo objeto vendrá un arreglo
+     * conteniendo las direccionesDefault. Obviamente, como se va a CREAR estos registros
+     * en la BD, el objeto JSON viene sin sus ids. El objeto enviado tendría esta forma:
+     * {
+     *     "nombre": "Anónimo",
+     *     "apellido": "Sistemas",
+     *     "createAt": "2022-11-18T20:05:00",
+     *     "direccionesDefault": [
+     *         {
+     *             "avenida": "Trujillo",
+     *             "manzana": "T",
+     *             "lote": 10
+     *         }
+     *         {
+     *             "avenida": "Arequipa",
+     *             "manzana": "NC",
+     *             "lote": 22
+     *         }
+     *     ]
+     * }
+     * ¿Por que se puede persistir el arreglo de DirecciónDefault, si estamos llamando al personaRepositorioDefault, que
+     * es de PersonaDefault?
+     * Es porque en PersonaDefault, que es nuestra clase principal, estamos definiendo en la relación @OneToMany el
+     * cascade del tipo = CascadeType.ALL, esto significa que cada vez que se cree o guarde un PersonaDefault,
+     * automáticamente creará a su dependiente, es decir a sus relacionados, en nuestro caso sería DirecciónDefault.
+     * Podemos crear una PersonaDefault junto a su DireccionDefault y lo persistirá.
+     */
     @PostMapping
     public ResponseEntity<?> guardarPersonaConDirecciones(@RequestBody PersonaDefault personaDefault) {
         return ResponseEntity.ok(this.personaRepositoryDefault.save(personaDefault));
