d7bb42835868457bc5f4db2cae1f8b1366378b46
Martín <magadiflo@gmail.com>
2022-11-19 16:55:23 -0500

    OneToOne - Unidireccional V1 - Listando credenciales y consigo viene su usuario

--- src/main/java/com/asociaciones/jpa/app/unidireccional/v1/OneToOne/resource/UsuarioResource.java
+++ src/main/java/com/asociaciones/jpa/app/unidireccional/v1/OneToOne/resource/UsuarioResource.java
@@ -2,11 +2,13 @@ package com.asociaciones.jpa.app.unidireccional.v1.OneToOne.resource;
 
 import com.asociaciones.jpa.app.unidireccional.v1.OneToOne.repository.ICredencialRepository;
 import com.asociaciones.jpa.app.unidireccional.v1.OneToOne.repository.IUsuarioRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/unidireccional/v1/one-to-one/usuarios")
+@RequestMapping(path = "/unidireccional/v1/one-to-one/credenciales")
 public class UsuarioResource {
 
     private final IUsuarioRepository usuarioRepository;
@@ -15,6 +17,11 @@ public class UsuarioResource {
     public UsuarioResource(IUsuarioRepository usuarioRepository, ICredencialRepository credencialRepository) {
         this.usuarioRepository = usuarioRepository;
         this.credencialRepository = credencialRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> listarCredenciales() {
+        return ResponseEntity.ok(this.credencialRepository.findAll());
     }
 
 
