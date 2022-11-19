f163a083608468fc8704840fb096b534e538d903
Martín <magadiflo@gmail.com>
2022-11-19 18:58:37 -0500

    ManyToMany - Unidireccional V1 - Guardando curso a un alumno existente

--- src/main/java/com/asociaciones/jpa/app/unidireccional/v1/ManyToMany/resource/AlumnoResource.java
+++ src/main/java/com/asociaciones/jpa/app/unidireccional/v1/ManyToMany/resource/AlumnoResource.java
@@ -1,11 +1,11 @@
 package com.asociaciones.jpa.app.unidireccional.v1.ManyToMany.resource;
 
+import com.asociaciones.jpa.app.unidireccional.v1.ManyToMany.entity.Alumno;
+import com.asociaciones.jpa.app.unidireccional.v1.ManyToMany.entity.Curso;
 import com.asociaciones.jpa.app.unidireccional.v1.ManyToMany.repository.IAlumnoRepository;
 import com.asociaciones.jpa.app.unidireccional.v1.ManyToMany.repository.ICursoRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/unidireccional/v1/many-to-many/alumnos")
@@ -23,6 +23,14 @@ public class AlumnoResource {
     @GetMapping
     public ResponseEntity<?> listarAlumnos() {
         return ResponseEntity.ok(this.alumnoRepository.findAll());
+    }
+
+    @PostMapping(path = "/{alumnoId}")
+    public ResponseEntity<?> agregarCurso(@RequestBody Curso curso, @PathVariable Long alumnoId) {
+        Alumno alumno = this.alumnoRepository.findById(alumnoId).orElseThrow();
+        alumno.getCursos().add(curso);
+
+        return ResponseEntity.ok(this.alumnoRepository.save(alumno));
     }
 
 
