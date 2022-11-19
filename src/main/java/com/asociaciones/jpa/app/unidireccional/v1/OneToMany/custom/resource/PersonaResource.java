8e1f41fbcce8ddf8a5292538fc69f03b8bbde9e5
Martín <magadiflo@gmail.com>
2022-11-18 22:44:45 -0500

    OneToMany - Unidireccional V1 - Buscando Persona dada en la BD y asignándole nuevas direcciones para su guardado

--- src/main/java/com/asociaciones/jpa/app/unidireccional/v1/OneToMany/custom/resource/PersonaResource.java
+++ src/main/java/com/asociaciones/jpa/app/unidireccional/v1/OneToMany/custom/resource/PersonaResource.java
@@ -1,10 +1,13 @@
 package com.asociaciones.jpa.app.unidireccional.v1.OneToMany.custom.resource;
 
+import com.asociaciones.jpa.app.unidireccional.v1.OneToMany.custom.entity.Direccion;
 import com.asociaciones.jpa.app.unidireccional.v1.OneToMany.custom.entity.Persona;
 import com.asociaciones.jpa.app.unidireccional.v1.OneToMany.custom.repository.IDireccionRepository;
 import com.asociaciones.jpa.app.unidireccional.v1.OneToMany.custom.repository.IPersonaRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/unidireccional/v1/one-to-many/custom/personas")
@@ -22,9 +25,18 @@ public class PersonaResource {
     public ResponseEntity<?> listarPersonas() {
         return ResponseEntity.ok(this.personaRepository.findAll());
     }
-    
+
     @PostMapping
     public ResponseEntity<?> guardarPersonaConDirecciones(@RequestBody Persona persona) {
+        return ResponseEntity.ok(this.personaRepository.save(persona));
+    }
+
+    @PostMapping(path = "/{personaId}")
+    public ResponseEntity<?> guardaDireccionesBuscandoUnaPersona(@RequestBody List<Direccion> direcciones, @PathVariable Long personaId) {
+        Persona persona = this.personaRepository.findById(personaId).orElseThrow();
+        direcciones.stream().forEach(direccion -> {
+            persona.getDirecciones().add(direccion);
+        });
         return ResponseEntity.ok(this.personaRepository.save(persona));
     }
 }
