e929fbb29f0a2a1a11682d9efd51c7e13ed175e1
Martín <magadiflo@gmail.com>
2022-11-18 20:09:18 -0500

    OneToMany - Unidireccional V1 - Guardar Persona con Direcciones

--- src/main/java/com/asociaciones/jpa/app/unidireccional/v1/OneToMany/resource/PersonaResource.java
+++ src/main/java/com/asociaciones/jpa/app/unidireccional/v1/OneToMany/resource/PersonaResource.java
@@ -1,11 +1,10 @@
 package com.asociaciones.jpa.app.unidireccional.v1.OneToMany.resource;
 
+import com.asociaciones.jpa.app.unidireccional.v1.OneToMany.entity.PersonaDefault;
 import com.asociaciones.jpa.app.unidireccional.v1.OneToMany.repository.IDireccionRepositoryDefault;
 import com.asociaciones.jpa.app.unidireccional.v1.OneToMany.repository.IPersonaRepositoryDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/unidireccional/v1/one-to-many/default/personas")
@@ -22,5 +21,10 @@ public class PersonaResource {
     @GetMapping
     public ResponseEntity<?> listarPersonas() {
         return ResponseEntity.ok(this.personaRepositoryDefault.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> guardarPersonaConDirecciones(@RequestBody PersonaDefault personaDefault) {
+        return ResponseEntity.ok(this.personaRepositoryDefault.save(personaDefault));
     }
 }
